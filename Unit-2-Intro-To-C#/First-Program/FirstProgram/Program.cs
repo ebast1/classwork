6f92959eaecb93e6f7508f279b7c257075a10b1b
ebast1 <ebast23@msu.idserve.net>
2025-03-27 21:57:55 -0400

    first commit

---
+++ Unit-2-Intro-To-C#/First-Program/FirstProgram/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace FirstProgram;
+
+// This program will ask for three numbers
+//      add them together and display the total
+
+// Programming Solution - Human Thinking
+// Identify the detailed steps
+
+// Ask for the numbers one at a time
+// Write-down each number as its given
+// Add the numbers together create a sum/total
+// Tell the requester the total
+
+// Now that we have the steps in out solution
+// Identify any data you need for the steps
+
+// 1. A place to write down each number - a variable in a program
+// 2. A place to hold the sum/total - a variable in a program
+// 3. A way to ask for the numbers - a method in a program (object.method())
+// 4. A way to receive the numbers - a method in a program (object.method())
+// 5. A way to report the sum/total - a method in a program (object.method())
+
+// Place the steps in the order and provide more detail if necessary
+// we need to solve the problem
+
+// 1. Ask for the numbers one at a time
+//      a. Tell whoever is giving us the numbers to give me the number first
+//      b. Receive the first number from sender
+//      c. Write down the first number
+//      d. Loop for each request (in this case its 3)
+
+// 2. Add the numbers together create a sum/total
+//      a. Add the first number to the sum/total
+//      b. Add the second number to the sum/total
+//      c. Add the third number to the sum/total
+
+// 3. Tell the requester the sum/total (display)
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         *  Define my data the program needs at the top of the program
+         *
+         *  Data may be defined anywhere before it's used, it's easier to find at the top of the program
+         *
+         *  To define a variable: data-type name = initial-value;
+         *
+         *  Commonly used data types in C#:
+         *
+         *      int - Numeric data that is a whole number between +/- 2 billion
+         *      long - A whole number that is greater than +/- 2 billion
+         *      double - Numeric value with decimal places
+         *      char - a single alphanumeric character
+         *      string - a series of alphanumeric characters (words or sentences)
+         *      bool - true or false value for conditions
+         *
+         *      examples:   10  - int (or long)
+         *                  1.0 - double
+         *                  'a' - char (note it is enclosed in ' ')
+         *                  "some words" - string (note it is enclosed in " ")
+         *
+         *  Variable names in C# are usually spelled in camelCase; Use _ to separate words
+         *  (camelCase means the first letter is in lowercase, and all other words start with an upper case letter)
+         *
+         *  It's always good to initialize variables when defining them so you know what is in them
+         */
+        
+        // Define the variables to hold the numbers we need to add
+
+        int number1 = 0;
+        int number2 = 0;
+        int number3 = 0;
+
+        //  Define a variable to hold the sum
+        
+        int sum = 0;
+        
+        Console.WriteLine("--- Starting Program ---");
+        
+        // Ask for the numbers one at a time using C# Console object
+        //      which represents the keyboard and screen
+        // Console.ReadLine() returns a string - cannot store in an int
+        
+        Console.WriteLine("Please enter a number: ");   // Asking for the number
+        string aLine;                                   // String to hold the line of input from the keyboard
+        aLine = Console.ReadLine();                     // Get a line from the keyboard
+        
+        // We need an int value to store our numbers
+        // Console.ReadLine() only returns a string
+        // So we need to convert the string from Console.ReadLine() to an int
+        // int.Parse(string) will convert a string into an int
+        
+        number1 = int.Parse(aLine);
+        
+        Console.WriteLine("--- Ending Program ---");
+    }
+}
---
+++ Unit-2-Intro-To-C#/First-Project/First-Project/Program.cs
@@ -0,0 +1,69 @@
+﻿
+// This is a comment - everything after the double slash is ignored
+
+/*
+ *  This is a block comment - everything after between the slash-asterisk
+ *                            and asterisk-slash is ignored
+ *
+ * this is userful if you have a lot to say or to deactivate code
+ *
+ 
+   The * at the start of the line is optional
+   
+ */
+
+// a namespace identifies a context in which a word is known 
+// allows the same name to be used in different contexts and to mean different things
+
+namespace First_Project;
+
+// Everything in C# is defined in a Class
+// a Class is a group of related things
+// a Class starts with the class keyword
+
+class Program
+    
+// { } surround blocks of code aka code-blocks
+
+{ // Start of the Code for the Class
+    
+    // Every application has a program called Main()
+    // a method is a self-contained set of code that performs a function
+    // also known as: a function or a program
+    // a method may return a value and accept data to process
+    // method signature identifies what the method returns, it's name and what it accepts
+    //      return-type name(data-it-accepts)
+    
+    // Main() is simply where every application starts
+    // Every application has one and only one Main() method
+    
+    // Below Main() is a method that returns void and accepts string[] called args
+    //
+    // void means it returns nothing
+    // static means there can only be one of these in this block or code (Program class)
+    
+    //     return
+    //     type   name(data-it-accepts)
+    static void   Main(string[] args) // method signature for Main()
+    
+    { 
+        // Start of the Code for Main()
+        // This code will display whatever is between the ("") on the screen
+        
+        // Console is an object defined by the C# to represent the screen
+        // An object has certain behaviors
+        // A behavior is something an object can do with data
+        // behaviors are implemented using methods
+        
+        // object.method(data) - object oriented programming
+        // 
+        // when you see a . after a name, the name to the left of the dot is probable an object
+        //                name(, the name is a method
+        //                name after the ., the name is usually a method
+        
+        // object.method(data-for-the-method-to-process)
+        Console.WriteLine("Hello, World!"); // EVERY C# STATEMENT ENDS WITH A ;
+        
+    } // End of the Code for Main()
+    
+} // End of the Code for the Class
