2690635a87527b2c469e81cc19d90e6179f6d83a
ebast1 <ebast23@msu.idserve.net>
2025-04-03 21:53:09 -0400

    finished class 4/3, string exercise

---
+++ Unit-2-Intro-To-C#/MethodsExamples/MethodsExamples/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace MethodsExamples;
+
+class Program
+{
+            /**************************************************************************
+             *
+             *  Demonstrate the use of Programmer defined methods
+             *
+             *  a method is a program that processes data and usually returns a value
+             *
+             *  a method starts with a method signature, which identifies the name of the method
+             *                                                            what type of data it returns
+             *                                                            what data it will accept for processing
+             *
+             *  Method signature:   return-type Name(data-it-will-accept)
+             *
+             *      return-type - the type of data returned from the method
+             *                    use void if the method does not return data
+             *
+             *      Name() - name of the method - method names are in PascalCase
+             *                                    PascalCase - the first letter of every word is capitalized
+             *
+             *      (data-it-will-accept) - data-type and name of any parameters
+             *
+             *      parameter is a piece of data passed/given to a method for it's processing
+             *
+             **************************************************************************/    
+     
+    // Every application has one and only one called Main()
+    //
+    // Main() method signature    
+    //
+    //
+    // static - indicates there can only be one of this in an application
+    //
+    //     return  
+    //     type    name(parameters-it-may-accept)
+    static void    Main(string[] args)  // Main() is a method that returns nothing and receives string[] called args
+    {
+        
+        Console.WriteLine("Hello, World!");
+        int mySum = Addem(4, 5);    // Send the numbers 4 and 5 to Addem to get the sum
+                                    // Addem() will be executed with the numbers 4 and 5
+                                    // it will return the sum
+                                    // which will be stored in the variable mySum
+
+        int yoda = 97;
+        int obiWon = 420; 
+        
+        Console.WriteLine("The sum of 4 and 5 is: " + mySum);
+        Console.WriteLine("The sum of 3 and 4 is: " + Addem(3, 4));
+        Console.WriteLine("The sum yoda and obiWon is: " + Addem(yoda, obiWon));    // Calling Addem() with two ints
+                                                                                    // Run the Addem() that takes two ints
+        Console.WriteLine("The sum of 3.1 and 4.5 is: " + Addem(3.1, 4.5)); // Calling Addem() with two doubles 
+                                                                            // Run the Addem() that takes two doubles
+        Console.WriteLine("The sum of 3 and 4.5 is: " + Addem(3, 4.5));     // Calling Addem() with an int and a double
+                                                                            // We don't have an Addem() that takes an int and a double
+                                                                            // It runs the Addem() that takes two doubles   
+                                                                            // C# converts the int into a double
+                                                                            // so now it can run the Addem() that takes doubles
+                                                                            
+    } // End of Main()
+    
+    // After a method you may define other methods that method can use
+    // "Helper Methods" - help a method perform a specific task
+    
+    // Define a method to receive two numbers and return the sum
+    //
+    // If a method will be used by a static method, it must be static itself
+    // Any method used by Main() must be static because Main() is static
+    
+    
+    //  Addem() is a method that receives two ints it's calling num1 and num2
+    //                      and returns the sum of the ints as an int
+    
+    //  return
+    //  type           name (parameters)
+        static int     Addem(int num1, int num2)
+        {
+            return num1 + num2;  // Add the two parameters and return the sum
+        }
+        static double  Addem(double num1, double num2)
+        {
+            return num1 + num2;  // Add the two parameters and return the sum
+        }
+
+} // End of Program class
--- Unit-2-Intro-To-C#/StringExamples-Frank-V1/StringExamples/Program.cs
+++ Unit-2-Intro-To-C#/StringExamples-Frank-V1/StringExamples/Program.cs
@@ -15,6 +15,8 @@ class Program
          *        you can't use < > to compare strings
          *        Beware: Early versions of C# do not allow use of == with strings
          *
+         * a method() is a program that process's data in an object/variable
+         * 
          * C# provides several methods to operate on strings:
          *
          *    Hey) - compare the string to left of the . to string inside () for equals
@@ -59,5 +61,49 @@ class Program
         {
             Console.WriteLine("string1 is NOT equal string2");
         }
+        
+        // Some methods that process strings
+         
+        // to see a list of all methods available to an object - just type objectName.
+        // .Length - returns the number of characters in a string
+        // .Contains(string) - returns true if the string given is inside the astring 
+        // .Substring(start-index, length) - extract characters from the string starting ar the start index for the number of characters indicated by the length
+        //      start-index starts at 0 not 1
+        //      whenever you see the word index - starts counting at 0
+        //      whenever you see the word position - starts counting at 1
+        // .StartsWith(string) - returns true if string starts with the specified chars (case-sensitive)
+        // .EndsWith(string) - returns true if string ends with the specified chars (case-sensitive)
+        // .Trim() - removes any leading and trailing spaces
+        
+        string sentence = "    My name is Frank and I attempt to teach C#    ";
+        Console.WriteLine("There are " + sentence.Length + " characters in the sentence");
+        
+        bool containsFrank = sentence.Contains("Frank"); // true if sentence contains "Frank"
+        Console.WriteLine("Contains frank: " + containsFrank);
+        
+        Console.WriteLine("Contains Marquise: " + sentence.Contains("Marquise"));
+
+        string nameInSentence = sentence.Substring(16, 5); // Extract the 5 characters starting at position 12
+        Console.WriteLine("Name is " + nameInSentence);
+        
+        Console.WriteLine("Does sentence start with My? " + sentence.StartsWith("My"));
+        Console.WriteLine("Does sentence start with my? " + sentence.StartsWith("my"));
+        Console.WriteLine("Does sentence end with C#? " + sentence.EndsWith("C#"));
+        
+        Console.WriteLine("Results when sentence.Trim() is used to remove leading and trailing spaces");
+        Console.WriteLine("Does sentence start with My? " + sentence.Trim().StartsWith("My"));
+        Console.WriteLine("Does sentence start with my? " + sentence.Trim().StartsWith("my"));
+        Console.WriteLine("Does sentence end with C#? " + sentence.Trim().EndsWith("C#"));
+        
+        // When C# sees a statement with chained operations (operations seperated by .)
+        // C# is going to Process the statement from left to right, one operation at a time
+        // Uses the result of the operation in the next operation
+        //
+        // sentence.Trim().EndsWith("C#")
+        //
+        // 1. sentence.Trim() - returns a string with the leading and trailing spaces removed
+        // 2. result-from-step-1.EndsWith() - returns true or false depending on if string actually ends with chars
+        //
+        
     } // End of Main()
 }  // End of class Program
