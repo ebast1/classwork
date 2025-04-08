79866c4f3f36ec8fe96c3ac7af3b6f162b799fa9
ebast1 <ebast23@msu.idserve.net>
2025-04-07 21:32:46 -0400

    monday apr 7

--- Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
+++ Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
@@ -7,39 +7,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Define the variables to hold the numbers we need to add
-        int number1 = 0; // Since we will be doing math it needs to be numeric
-        int number2 = 0; // Since we will be doing math it needs to be numeric
-        int number3 = 0; // Since we will be doing math it needs to be numeric
-
+        // Define an array to hold 3 numbers
+        int[] numbers = new int[5];  //  numbers.Length = 3; 2 is the largest allowable index
+        
         // Define a variable to hold the sum of the numbers
         int sum = 0;
 
         Console.WriteLine("--- Starting program ---");
+        
+        // Normally we process an array from beginning to end 
+        // (from the first element to the last element one at a time)
+        // 
+        // a for loop will loop through a process counting as it does so
+        // a for-loop is an excellent tool for processing an array from start to end
 
-        // Ask for the numbers one at time using the C# Console object
-        //     which represents the keyboard and screen
-        // Console.ReadLine() returns a string from keyboard - cannot store in an int
-        Console.WriteLine("Please enter a number:  "); // Asking for the number
-        string theResponse; // Define a string to hold the line of input from the keyboard
-        theResponse = Console.ReadLine(); // Get a line from the keyboard
-      
-        // int.Parse(string) will convert a string to an int
-        number1 = int.Parse(theResponse);
+        // standard syntax; for(i=0; i< arrayName.Length; i++)
+        //
+        // arrayName.Length represents the number of elements in the array
+        
+        //  index start at 0     loop as long as the index      increment the index
+        //                       is not outside the array       for each loop
+        for (int i = 0;          i < numbers.Length;            i++) // i = 0, 1, 2 inside the loop and be 3 when it exits the loop
+        {
+            
+            Console.WriteLine("Please enter a number:  "); // Asking for the number
+            numbers[i] = int.Parse(Console.ReadLine());  // get a string from the keyboard
+                                                         // convert it to an integer
+                                                         // store it in numbers[i]
+                                                         
+        }   
 
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number2 = int.Parse(theResponse);
+        // Verify the array received the numbers correctly
+        // Go through the array one element at a time and display the element
 
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number3 = int.Parse(theResponse);
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Element #" +i+ " is : " + numbers[i]); 
+            sum += numbers[i];
+            
+        }                                            
+        
 
         // Add the numbers together create a sum/total
-        sum = number1 + number2 + number3;
+        // sum = number1 + number2 + number3 + number4 + number5;
        
         // Tell the requester the sum/total (display)
         Console.WriteLine("The sum is: " + sum);
+        
+        Console.WriteLine("The average of the numbers is: " + (double) sum/numbers.Length);
 
         Console.WriteLine("--- Ending program ---");
     }
