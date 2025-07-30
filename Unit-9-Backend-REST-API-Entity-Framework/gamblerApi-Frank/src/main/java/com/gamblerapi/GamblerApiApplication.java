e2289eac02ba35b597a7c3abfb373f9e05415bdf
Frank Fella <Frank@CyberAcademy.com>
2025-07-30 15:26:38 -0700

    Instructor Push 07/30/2025 15:26:38

---
+++ Unit-9-Backend-REST-API-Entity-Framework/gamblerApi-Frank/src/main/java/com/gamblerapi/GamblerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamblerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamblerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamblerApiApplication.class, args);
+	}
+
+}
