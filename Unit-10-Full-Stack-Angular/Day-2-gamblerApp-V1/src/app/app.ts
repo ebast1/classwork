51a7ce80eca8c68c5f857135475e27ea7d930748
Frank Fella <Frank@CyberAcademy.com>
2025-08-14 14:36:58 -0700

    Instructor Push 08/14/2025 14:36:58

---
+++ Unit-10-Full-Stack-Angular/Day-2-gamblerApp-V1/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomePage } from "./components/home-page/home-page";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomePage],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'gamblerApp';
+}
